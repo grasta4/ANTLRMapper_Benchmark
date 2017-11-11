ce71c98a2d018d7ed8287f7a2cc74c30c3fb3e90
Buonanno <grasta4@gmail.com>
2017-11-11 01:07:52 +0100

    Major Fixes

--- src/core/Benchmark.java
+++ src/core/Benchmark.java
@@ -1,76 +1,57 @@
 package core;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.File;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import it.unical.mat.embasp.specializations.dlv.DLVAnswerSets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.stream.Stream;
 
 public class Benchmark {
+	private static final void makeSolutions(final String solver, final String problem) throws InterruptedException, IOException {
+		assert (solver.equals("clingo") || solver.equals("dlv") || solver.equals("dlv2")) && (problem.equals("hanoi_tower") || problem.equals("labyrinth") || problem.equals("myTests") || problem.equals("solitaire")); 
+	
+		final LinkedList<String> instances = new LinkedList <> ();
+		
+		try(Stream<Path> paths = Files.walk(Paths.get("rsc/problems/asp/" + problem + "/instances"))) {
+		    paths.filter(Files::isRegularFile).forEach(element -> instances.add(element.getFileName().toString()));
+		}
+		
+		for(final String instance : instances) {
+			final LinkedList<String> lines = new LinkedList <> ();
+			final Process process = new ProcessBuilder("rsc/" + solver, "rsc/problems/asp/" + problem + "/instances/" + instance, "rsc/problems/asp/" + problem + "/encoding.asp").start();
+			
+			System.out.print("Making output of '" + solver + "' with instance '" + instance + "' of '" + problem + "' -> ");
+			process.waitFor();
+
+			try(final BufferedReader processOutput = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+				String line;
+		
+				while((line = processOutput.readLine()) != null)
+					lines.add(line);
+			}
+		
+			final File file = new File("rsc/problems/asp/" + problem + "/dlvOutputs/" + instance.charAt(0));
+		
+			file.getParentFile().mkdir();
+		
+			try(final PrintWriter writer = new PrintWriter(file)) {
+				lines.forEach(element -> writer.println(element));
+			}
+			
+			System.out.println("DONE.");
+		}
+	}
+	
 	public static void main(String[] args) {
-		/*
-		 * This portion reads the output file of the solver and stores it in a String.
-		 */
-		String test = "";
-		
-		try(final BufferedReader bufferedReader = new BufferedReader(new FileReader("rsc/DLVTest0.txt"))) {
-			String line;
-			
-			while((line = bufferedReader.readLine()) != null)
-	            test += line + "\n";
-	    } catch (final IOException exception) {
-	        exception.printStackTrace();
-	    }
-		/*
-		 * 
-		 */
-		
-		/*
-		 * This portion counts the values in the .csv in order to determine if a carriage return is required (there must be one every ten values)
-		 */
-		String[] values = null;
-		
-	    try(final BufferedReader bufferedReader = new BufferedReader(new FileReader("rsc/results.csv"))) {
-	    	String line;
-			
-	    	while((line = bufferedReader.readLine()) != null)
-	            values = line.split(";");
-	    } catch (final IOException exception) {
-	        exception.printStackTrace();
-	    }
-		/*
-		 * 
-		 */
-	    
-	    /*
-	     * This portion does the actual testing.
-	     */
-	    final DLVAnswerSets dlvAnswerSets = new DLVAnswerSets(test, "");
-		final long start = System.nanoTime();
-		
-		dlvAnswerSets.getAnswersets();
-		
-		final long end = System.nanoTime();
-		/*
-		 * 
-		 */
-		
-		/*
-		 * This portion stores the value in the .csv file. 
-		 */
-		final String carriageReturn = (values == null || values.length % 10 != 0) ? "" : "\n";
-		
-		try(final FileWriter fileWriter = new FileWriter("rsc/results.csv", true); final BufferedWriter bufferedWriter = new BufferedWriter(fileWriter); final PrintWriter out = new PrintWriter(bufferedWriter)) {
-	        out.print(carriageReturn + ((end - start) / 1000000) + ";");
-		} catch (final IOException exception) {
+		try {
+			makeSolutions("dlv", "myTests");
+		} catch (final InterruptedException | IOException exception) {
 			exception.printStackTrace();
 		}
-		/*
-		 *
-		 */
-		
-		dlvAnswerSets.getAnswersets().forEach(System.out::println);
 	}
 }
